81fcf9b7e2b733e3f98802c9b918214dada9327c
GroverW <cheesemaker@gmail.com>
2019-11-29 10:19:33 -0800

    Initial commit for Bookshelf project. Index.html started with bookshelf containers and styling for containers.

---
+++ projects/bookshelf/script.js
@@ -0,0 +1,8 @@
+var Book = function(title,author,numPages,isRead,summary,rating) {
+    this.title = title;
+    this.author = author;
+    this.numPages = numPages;
+    this.isRead = isRead;
+    this.summary = summary;
+    this.rating = rating;
+}
