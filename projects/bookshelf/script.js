37fe0f5c05319bd8f227ac05f4f48b8bd3fc6956
GroverW <cheesemaker@gmail.com>
2019-12-02 16:23:59 -0800

    Added click event handlers for shelves and books to make sure they are independent and select the correct elements. Added bookshelf array.

--- projects/bookshelf/script.js
+++ projects/bookshelf/script.js
@@ -6,3 +6,30 @@ var Book = function(title,author,numPages,isRead,summary,rating) {
     this.summary = summary;
     this.rating = rating;
 }
+
+const SHELVES = 2;
+const STACKS_PER_SHELF = 3;
+const BOOKS_PER_STACK = 4;
+
+let bookShelf = new Array(SHELVES).fill(null)
+                    .map((v) => Array(STACKS_PER_SHELF).fill(null)
+                    .map((v) => Array(0)));
+
+let stacks = document.querySelectorAll('.shelf_stack');
+
+stacks.forEach((shelf) => {
+    shelf.addEventListener('click',() => {
+        let coords = shelf.id.split('_');
+
+        console.log(coords[0],coords[1]);
+    });
+});
+
+let books = document.querySelectorAll('.book1, .book2, .book3, .book4');
+
+books.forEach((book) => {
+    book.addEventListener('click',(event) => {
+        event.stopPropagation();
+        console.log('book!');
+    });
+});
