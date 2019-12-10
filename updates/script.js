33e6573001677144aa31b28acfda2aa7f0ec3608
GroverW <cheesemaker@gmail.com>
2019-12-09 20:52:12 -0800

    Adding to fetch files via Ajax request.

---
+++ updates/script.js
@@ -0,0 +1,27 @@
+let updateList = [
+    '12-05-2019.html',
+    '11-27-2019.html',
+    '11-22-2019.html',
+    '11-18-2019.html',
+    '11-05-2019.html',
+    '11-04-2019.html'
+]
+
+let updateContent = document.querySelector('#update_content');
+
+updateList.forEach(file => {
+    fetch(`update-files/${file}`)
+    .then(response => {
+        return response.text();
+    })
+    .then(html => {
+        let parser = new DOMParser();
+
+        let doc = parser.parseFromString(html,"text/html");
+        let update = doc.querySelector('.update');   
+        updateContent.appendChild(update);
+    })
+    .catch(err => {
+        console.log('Failed to fetch page: ' + err);
+    });
+});
