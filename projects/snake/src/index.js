584103d81561f1cd8ea09d75ad1e4dc4cbe022d9
GroverW <cheesemaker@gmail.com>
2019-12-20 14:37:37 -0800

    Moved snakeObjct to index.js.

--- projects/snake/src/index.js
+++ projects/snake/src/index.js
@@ -1,5 +1,30 @@
-const testsWork = () => {
-    return 'Ready!';
+const pubSub = require('./pubSub');
+
+const snakeObject = () => {
+    let snake = [
+        [0,0],
+        [0,1],
+        [0,2]
+    ];
+    let head = snake[snake.length - 1];
+
+    const getSnake = () => snake;
+
+    const updateHead = () => head = snake[snake.length - 1];
+
+    const moveSnake = (direction) => {
+        snake.shift();
+        snake.push([
+            head[0] + direction[0],
+            head[1] + direction[1]
+        ]);
+
+        updateHead();
+    }
+
+    return { getSnake, moveSnake };
 }
 
-module.exports = testsWork;
+module.exports = {
+    snakeObject: snakeObject()
+};
--- projects/snake/src/unit-tests/index.test.js
+++ projects/snake/src/unit-tests/index.test.js
@@ -1,5 +1,19 @@
-const testsWork = require('../index');
+const tests = require('../index');
 
-test('TDD Ready!',() => {
-    expect(testsWork()).toBe('Ready!');
-})
+test('Set Default Snake',() => {
+    expect(tests.snakeObject.getSnake()).toEqual([
+        [0,0],
+        [0,1],
+        [0,2]
+    ]);
+});
+
+test('Move Snake Down',() => {
+    tests.snakeObject.moveSnake([0,1]);
+
+    expect(tests.snakeObject.getSnake()).toEqual([
+        [0,1],
+        [0,2],
+        [0,3]
+    ]);
+});
