5fc482d67ea66843a0145f0f73a8ba23e4ca2fd8
GroverW <cheesemaker@gmail.com>
2020-04-20 09:22:53 -0700

    added prism code highlighting

---
+++ updates/updates.js
@@ -0,0 +1,39 @@
+const updateList = [
+    '04-18-2020.html',
+    '12-17-2019.html',
+    '12-12-2019.html',
+    '12-05-2019.html',
+    '11-27-2019.html',
+    '11-22-2019.html',
+    '11-18-2019.html',
+    '11-05-2019.html',
+    '11-04-2019.html'
+]
+
+const updateContent = document.querySelector('#update_content');
+
+const loadUpdates = async list => {
+    try {
+        const updatePromises = list.map(item => fetch(`update-files/${item}`))
+        const updates = await Promise.all(updatePromises)
+        
+        updates.forEach(async element => {
+            const text = await element.text();
+            const parser = new DOMParser();
+            const html = parser.parseFromString(text,"text/html");
+            const update = html.querySelector('.update');
+            Prism.highlightAllUnder(update, true);
+            updateContent.appendChild(update)
+        })
+
+        console.log(Prism);
+    } catch (err) {
+        console.log(`Failed to fetch page: ${err}`);
+    }
+}
+
+loadUpdates(updateList)
+
+
+
+
