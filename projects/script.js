f199de5fb29d746ac6cabf1c3f34f35c5370b3dd
GroverW <cheesemaker@gmail.com>
2020-07-10 10:30:40 -0700

    updated script with tetris, jobly, warbler

--- projects/script.js
+++ projects/script.js
@@ -1,48 +1,52 @@
 let projectList = [
-    {folder: 'memory-game', image: 'memory_image.png', caption: 'Memory Game'},
-    {folder: 'bookshelf', image: 'book_image.png', caption: 'Bookshelf'},
-    {folder: 'meme-generator', image: 'meme_image.png', caption: 'Meme Generator'},
-    {folder: 'calculator', image: 'calc_image.png', caption: 'Calculator'},
-    {folder: 'etch-a-sketch', image: 'sketch_image.png', caption: 'Etch-A-Sketch'},
-    {folder: 'rock-paper-scissors', image: 'rpc_image.png', caption: 'Rock-Paper-Scissors'}
+    { link: 'https://test-driven-tetris.herokuapp.com', image: 'tetris_image.png', caption: 'Test-Driven-Tetris' },
+    { link: 'https://r14-warbler.herokuapp.com', image: 'warbler_image.png', caption: 'Warbler' },
+    { link: 'https://r14-jobly-frontend.herokuapp.com', image: 'jobly_image.png', caption: 'Jobly' },
+    { folder: 'memory-game', image: 'memory_image.png', caption: 'Memory Game' },
+    { folder: 'bookshelf', image: 'book_image.png', caption: 'Bookshelf' },
+    { folder: 'meme-generator', image: 'meme_image.png', caption: 'Meme Generator' },
+    { folder: 'calculator', image: 'calc_image.png', caption: 'Calculator' },
+    { folder: 'etch-a-sketch', image: 'sketch_image.png', caption: 'Etch-A-Sketch' },
+    { folder: 'rock-paper-scissors', image: 'rpc_image.png', caption: 'Rock-Paper-Scissors' }
 ]
 
-let ProjectThumbNail = function(imageFolder,imageName,captionName) {
-    this.imageFolder = imageFolder;
-    this.imageName = imageName;
-    this.captionName = captionName;
+let ProjectThumbNail = function ({ folder, link, image, caption }) {
+    this.href = folder ? `${folder}/index.html` : link;
+    this.src = `images/${image}`;
+    this.caption = caption;
 }
 
-ProjectThumbNail.prototype.addToProjects = function(selector) {
-    let projectLink = document.createElement('a');
-    projectLink.href = `${this.imageFolder}/index.html`;
-    projectLink.classList.add('project_link');
+ProjectThumbNail.prototype.addToProjects = function (selector) {
+    let thumbnail = document.createElement('a');
 
-    let projectFigure = document.createElement('figure');
+    thumbnail.href = this.href;
+    thumbnail.classList.add('project_link');
 
-    let projectImg = document.createElement('img');
-    projectImg.src = `images/${this.imageName}`;
-    projectImg.classList.add('thumb_image');
+    let figure = document.createElement('figure');
 
-    let projectCaption = document.createElement('figcaption');
-    projectCaption.innerText = this.captionName;
+    let image = document.createElement('img');
+    image.src = this.src;
+    image.classList.add('thumb_image');
 
-    projectFigure.appendChild(projectImg);
-    projectFigure.appendChild(projectCaption);
+    let caption = document.createElement('figcaption');
+    caption.innerText = this.caption;
 
-    projectLink.appendChild(projectFigure);
+    figure.appendChild(image);
+    figure.appendChild(caption);
 
-    selector.appendChild(projectLink);
+    thumbnail.appendChild(figure);
+
+    selector.appendChild(thumbnail);
 }
 
+const numCols = gridCols.length;
 let gridCols = document.querySelectorAll('.grid_col');
-let numCols = gridCols.length;
 let currCol = 0;
 
-for(let project of projectList) {
-    let projectThumbNail = new ProjectThumbNail(project.folder,project.image,project.caption);
-    
+for (let project of projectList) {
+    let projectThumbNail = new ProjectThumbNail(project);
+
     projectThumbNail.addToProjects(gridCols[currCol]);
-    
+
     currCol = (currCol + 1) % numCols;
 }
